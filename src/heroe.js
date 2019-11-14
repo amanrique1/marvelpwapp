4aef28a7e246388d64be63a1dd42d992937f5836
Andres3 <a.manrique@uniandes.edu.co>
2019-11-13 21:24:47 -0500

    finished

--- src/heroe.js
+++ src/heroe.js
@@ -6,18 +6,18 @@ export default class Heroe extends Component {
         this.state = {
             name: this.props.heroe.name,
             description: this.props.heroe.description,
-            image: this.props.heroe.thumbnail.path
+            image: this.props.heroe.thumbnail.path + "/standard_large." + this.props.heroe.thumbnail.extension
         }
     }
 
 
     render() {
         return (
-            <div class="card" style="width: 18rem;">
-                <img src={this.state.image} class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">{this.state.name}</h5>
-                    <p class="card-text">{this.state.description}</p>
+            <div className="card bg-light mb-3 col-md-4">
+                <img src={this.state.image} className="card-img-top" alt={this.state.image+" image"} height="200" width="200" />
+                <div className="card-body">
+                    <h5 className="card-title">{this.state.name}</h5>
+                    <p className="card-text">{this.state.description}</p>
                 </div>
             </div>
         )
--- src/heroesList.js
+++ src/heroesList.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import Heroe from './heroe';
 
 class HeroesList extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            heroes: []
-        };
+    state = {
+        heroes: []
     }
+
 
     componentDidMount() {
         if (!navigator.onLine) {
             if (localStorage.getItem('heroes') === null)
                 this.setState({ heroes: [] })
             else
-                this.setState({ heroes: localStorage.getItem('heroes') });
+            {
+                console.log(localStorage.getItem('heroes'));
+                this.setState({ heroes: JSON.parse(localStorage.getItem('heroes') )});
+            }
         }
 
         fetch("https://gateway.marvel.com:443/v1/public/characters?ts=marvelapi&hash=7f63179ed893e0030792be6ed0674293&apikey=0b16b58a5f077c37421ce5a610433ca2")
@@ -22,15 +23,21 @@ class HeroesList extends Component {
                 return res.json();
             }).then(res => {
                 this.setState({ heroes: res.data.results });
-                let save=this.state.heroes.slice(0,4);
-                localStorage.setItem('heroes', save);
+                let save = res.data.results.slice(0, 6);
+                localStorage.setItem('heroes', JSON.stringify(save));
+                console.log("carga:"+localStorage.getItem('heroes'));
+                console.log(this.state.heroes);
             });
     }
 
     render() {
         return (
-            <div className="row">
-                {this.state.heroes.map((e, i) => <Heroe key={i} heroe={e} />)}
+            <div>
+                <div className="container">
+                    <div className="row mt-4">
+                    {this.state.heroes.map((e, i) => <Heroe key={i} heroe={e} />)}
+                    </div>
+                </div>
             </div>
         );
     }
